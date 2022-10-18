01358a09f0f2649d750a2d59ac47163e08e44d43
Florent-Vasseur <vasseurflorent@gmail.com>
2022-10-18 17:42:42 +0200

    quete ok

--- public/index.php
+++ public/index.php
@@ -6,6 +6,5 @@ require_once __DIR__ . '/../config/twig.php';
 
 $name = 'Wilder';
 
-echo $twig->render('home.html.twig', ['name' => $name]);
 
-require_once __DIR__ . '/../public/products.php';
+echo $twig->render('home.html.twig', ['name'=>$name]);
--- public/products.php
+++ public/products.php
@@ -9,5 +9,4 @@ $products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
 echo $twig->render('products.html.twig', ['products' => $products]);
 
 
-
 ?>
